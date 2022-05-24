248653d438fb6c38e3df21e65ca598d438a2a0f9
Janos Meny <janos.meny@googlemail.com>
2022-05-24 22:29:04 +0200

    split mesh into seperate module

---
+++ src/mesh.rs
@@ -0,0 +1,531 @@
+
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use std::path::Path;
+use std::ffi::OsStr;
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+use std::ops;
+
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+}
+
+impl ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, p: Vec3) -> Vec3 {
+        Vec3::new(self.x + p.x, self.y + p.y, self.z + p.z)
+    }
+}
+
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, p: Vec3) -> Vec3 {
+        Vec3::new(self.x * p.x, self.y * p.y, self.z * p.z)
+    }
+}
+
+impl ops::Div<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, p: Vec3) -> Vec3 {
+        Vec3::new(self.x / p.x, self.y / p.y, self.z / p.z)
+    }
+}
+
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, p: Vec3) -> Vec3 {
+        Vec3::new(self.x - p.x, self.y - p.y, self.z - p.z)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Face {
+    id : usize,
+    mesh : RcMesh,
+}
+
+impl Face {
+    fn new(id: usize, mesh: RcMesh) -> Face {
+        Face { id, mesh }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Edge {
+    id : usize,
+    mesh : RcMesh,
+}
+
+impl Edge {
+    fn new(id: usize, mesh: RcMesh) -> Edge {
+        Edge { id, mesh }
+    }
+
+    fn halfedge(&self) -> Halfedge {
+        Halfedge::new(self.id * 2, self.mesh.clone())
+    }
+
+    fn endpoint1(&self) -> Vertex {
+        self.halfedge().from()
+    }
+
+    fn endpoint2(&self) -> Vertex {
+        self.halfedge().to()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Vertex {
+    id : usize,
+    mesh : RcMesh,
+}
+
+impl Vertex {
+    fn new(id: usize, mesh: RcMesh) -> Vertex {
+        Vertex { id, mesh }
+    }
+
+    fn halfedge(&self) -> Option<Halfedge> {
+        let id = self.mesh.data.borrow().vertex_halfedge[self.id];
+        if id == std::u32::MAX { None } else { Some(Halfedge::new(id as usize, self.mesh.clone())) }
+    }
+
+    fn halfedge_checked(&self) -> Halfedge {
+        self.halfedge().expect("Vertex::halfedges() called on isolated vertex")
+    }
+
+    fn outgoing_halfedges(&self) -> OutgoingHalfedgeIterator {
+        let he = self.halfedge();
+        OutgoingHalfedgeIterator { begin: he.clone(), current: he, started: false }
+    }
+
+    fn is_boundary(&self) -> bool {
+        match self.halfedge() {
+            Some(he) => he.is_boundary(),
+            None => true,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Halfedge {
+    id : usize,
+    mesh : RcMesh,
+}
+
+impl Halfedge {
+    fn new(id: usize, mesh: RcMesh) -> Halfedge {
+        Halfedge { id, mesh }
+    }
+
+    fn edge(&self) -> Edge {
+        Edge::new(self.id / 2, self.mesh.clone())
+    }
+
+    fn face(&self) -> Option<Face> {
+        let id = self.mesh.data.borrow().halfedge_face[self.id];
+        if id == std::u32::MAX {
+            None
+        } else {
+            Some(Face::new(id as usize, self.mesh.clone()))
+        }
+    }
+
+    fn twin(&self) -> Halfedge {
+        Halfedge::new(self.id ^ 1, self.mesh.clone())
+    }
+
+    fn next(&self) -> Halfedge {
+        let id = self.mesh.data.borrow().next_halfedge[self.id] as usize;
+        Halfedge::new(id, self.mesh.clone())
+    }
+
+    fn prev(&self) -> Halfedge {
+        let id = self.mesh.data.borrow().prev_halfedge[self.id] as usize;
+        Halfedge::new(id, self.mesh.clone())
+    }
+
+    fn from(&self) -> Vertex {
+        let id = self.mesh.data.borrow().halfedge_vertex[self.id] as usize;
+        Vertex::new(id, self.mesh.clone())
+    }
+
+    fn to(&self) -> Vertex {
+        self.twin().from()
+    }
+
+    fn is_boundary(&self) -> bool {
+        self.face().is_none()
+    }
+}
+
+struct OutgoingHalfedgeIterator{
+    begin : Option<Halfedge>,
+    current : Option<Halfedge>,
+    started : bool
+}
+
+impl Iterator for OutgoingHalfedgeIterator {
+  type Item = Halfedge;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.started {
+        let next = self.current.as_ref().expect("started, so begin is not None").twin().next();
+        if next == *self.begin.as_ref().expect("started, so begin is not None") {
+            return None;
+        }
+        self.current = Some(next);
+        return self.current.clone();
+    } else {
+        if let Some(begin) = self.begin.as_ref() {
+          self.started = true;
+          let current = begin.clone();
+          self.current = Some(current.clone());
+          return Some(current);
+        }
+        return None;
+    }
+  }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct MeshData {
+    next_halfedge : Vec<u32>,
+    prev_halfedge : Vec<u32>,
+    halfedge_face : Vec<u32>,
+    halfedge_vertex : Vec<u32>,
+
+    face_halfedge : Vec<u32>,
+    vertex_halfedge : Vec<u32>,
+
+    points : Vec<Vec3>,
+}
+
+impl MeshData {
+
+    fn new() -> MeshData {
+        MeshData { next_halfedge : vec![], 
+                prev_halfedge : vec![], 
+                halfedge_face : vec![], 
+                halfedge_vertex : vec![], 
+                face_halfedge : vec![], 
+                vertex_halfedge : vec![], 
+                points : vec![] 
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Mesh {
+   data : RefCell<MeshData>,
+}
+
+type RcMesh = Rc<Mesh>;
+
+fn get_extension_from_filename(filename: &str) -> Option<&str> {
+    Path::new(filename)
+        .extension()
+        .and_then(OsStr::to_str)
+}
+
+fn find_halfedge(v : &Vertex, w : &Vertex) -> Option<Halfedge> {
+    for he in v.outgoing_halfedges() {
+        if he.to() == *w {
+            return Some(he);
+        }
+    }
+    None
+}
+
+fn find_free_gap(he : Halfedge) -> Halfedge {
+    let mut he = he;
+    loop {
+        he = he.next().twin();
+        if !he.is_boundary() {
+            return he;
+        }
+    }
+}
+
+impl Mesh {
+    fn new() -> Mesh {
+        Mesh { data : RefCell::new(MeshData::new()) }
+    }
+
+    fn new_shared() -> RcMesh {
+        Rc::new(Mesh::new())
+    }
+    
+    pub fn load(path : &str) -> Result<RcMesh, io::Error> {
+
+        if get_extension_from_filename(path) == Some("obj") {
+            let file = File::open(path)?;
+            let reader = BufReader::new(file);
+            let mesh = Mesh::new_shared();
+            let mut faces = Vec::new();
+            for line in reader.lines() {
+                let line = line?;
+                let mut items = line.split_whitespace();
+                match items.next() {
+                    Some("v") => {
+                        let x = items.next().unwrap().parse::<f64>().unwrap();
+                        let y = items.next().unwrap().parse::<f64>().unwrap();
+                        let z = items.next().unwrap().parse::<f64>().unwrap();
+                        mesh.add_vertex(Vec3::new(x, y, z));
+                    },
+                    Some("f") => {
+                        let i1 = items.next().unwrap().parse::<usize>().unwrap();
+                        let i2 = items.next().unwrap().parse::<usize>().unwrap();
+                        let i3 = items.next().unwrap().parse::<usize>().unwrap();
+                        let vs = [Vertex { id : i1 - 1, mesh : mesh.clone() },
+                                  Vertex { id : i2 - 1, mesh : mesh.clone() },
+                                  Vertex { id : i3 - 1, mesh : mesh.clone() }];
+                        faces.push(vs);
+                    },
+                    _ => (),
+                }
+            }
+
+            let mut count = 0;
+            for face in faces {
+                println!("face {}", count);
+                mesh.add_face(&face).unwrap();
+                count += 1;
+            }
+
+            println!("Loaded mesh with {} vertices and {} faces", mesh.num_vertices(), mesh.num_faces());
+            return Ok(mesh);
+        }
+
+        Err(io::Error::new(io::ErrorKind::Other, "Unsupported file type"))
+    }
+
+    fn add_face(self : &Rc<Self>, vs : &[Vertex]) -> Option<Face> {
+        let n = vs.len();
+        assert!(n > 2);
+
+        let mut edge_data : Vec<(Option<Halfedge>, bool, bool)> = Vec::with_capacity(n);
+
+        // test for errors
+        for i in 0..n {
+            let v = &vs[i];
+            if !v.is_boundary() {
+                // Nonmanifold vertex
+                return None;
+            }
+            
+            let w = &vs[(i + 1) % n]; 
+            let he = find_halfedge(&v, &w);
+            if he.is_some() && !he.as_ref().unwrap().is_boundary() {
+                // Nonmanifold edge
+                return None;
+            }
+            let is_new = he.is_none();
+            let needs_adjustment = false;
+            edge_data.push((he, is_new, needs_adjustment));
+        }
+
+        let mut next_cache = Vec::with_capacity(6*n);
+
+        // re-link patches if necessary
+        for i in 0..n {
+            let j = (i + 1) % n;
+            
+            if let (Some(inner_prev), Some(inner_next)) = (&edge_data[i].0, &edge_data[j].0) {
+                if inner_prev.next() == *inner_next {
+                    let outer_prev = inner_next.twin();
+                    let outer_next = inner_prev.twin();
+
+                    let boundary_prev = find_free_gap(outer_prev);
+                    let boundary_next = boundary_prev.next();
+
+                    if boundary_prev == *inner_prev {
+                        // Patch re-linking failed
+                        return None;
+                    }
+
+                    assert!(boundary_prev.is_boundary());
+                    assert!(boundary_next.is_boundary());
+
+                    let patch_start = inner_prev.next();
+                    let patch_end = inner_next.prev();
+
+                    // relink patch
+                    next_cache.push((boundary_prev, patch_start));
+                    next_cache.push((patch_end, boundary_next));
+                    next_cache.push((inner_prev.clone(), inner_next.clone()));
+                }
+            }
+        }
+
+        // Create missing edges 
+        for i in 0..n {
+            let j = (i + 1) % n;
+            let is_new = edge_data[i].1;
+            if is_new {
+                edge_data[i].0 = Some(self.new_edge(&vs[i], &vs[j]));
+            }
+        }
+
+        // Create the face
+        let f = self.new_face();
+
+        // setup halfedges
+        for i in 0..n {
+            let j = (i + 1) % n;
+
+            let v = &vs[i];
+
+            let (inner_prev, prev_is_new, _) = edge_data[i].clone();
+            let (inner_next, next_is_new, _) = edge_data[j].clone();
+
+            let inner_prev = inner_prev.expect("At this point, all new halfedges should be created");
+            let inner_next = inner_next.expect("At this point, all new halfedges should be created");
+
+            // set outer links
+            if prev_is_new || next_is_new {
+
+                let outer_prev = inner_next.twin();
+                let outer_next = inner_prev.twin();
+
+                match (prev_is_new, next_is_new) {
+                    (true, false) => {
+                        let boundary_prev = inner_next.prev();
+                        next_cache.push((boundary_prev, outer_next.clone()));
+                        self.set_vertex_halfedge(v, &outer_next);
+                    },
+                    (false, true) => {
+                        let boundary_next = inner_prev.next();
+                        next_cache.push((outer_prev, boundary_next.clone()));
+                        self.set_vertex_halfedge(v, &boundary_next);
+                    },
+                    (true, true) => {
+                        if self.data.borrow().vertex_halfedge[v.id] == std::u32::MAX {
+                            self.set_vertex_halfedge(v, &outer_next);
+                            next_cache.push((outer_prev, outer_next));
+                        }
+                        else {
+                            let boundary_next = v.halfedge().expect("exists by construction");
+                            let boundary_prev = boundary_next.prev();
+                            next_cache.push((boundary_prev, outer_next));
+                            next_cache.push((outer_prev, boundary_next));
+                        }
+                    },
+                    _ => unreachable!(),
+                }
+
+                // set inner link
+                next_cache.push((inner_prev, inner_next));
+            }
+            else {
+                edge_data[j].2 = v.halfedge().expect("vertex halfedge was set when generating edges") == inner_next;
+            }
+
+            // set face handle
+            self.set_halfedge_face(edge_data[i].0.as_ref().unwrap(), &f);
+        }
+
+        for (he1, he2) in next_cache {
+            self.set_next_and_prev(&he1, &he2);
+        }
+
+        for ((_, _, needs_adjustment), v) in edge_data.iter().zip(vs) {
+            if *needs_adjustment {
+                self.adjust_outgoing_halfedge(v);
+            }
+        }
+
+        Some(f)
+    }
+
+    fn add_vertex(self : &Rc<Self>, p : Vec3) -> Vertex {
+        let mut data = self.data.borrow_mut();
+        let id = data.points.len();
+        data.vertex_halfedge.push(std::u32::MAX);
+        data.points.push(p);
+        Vertex {
+            id,
+            mesh : self.clone(), 
+        }
+    }
+
+    fn flip(&mut self, e : &Edge) {
+
+    }
+
+    fn num_vertices(&self) -> usize {
+        self.data.borrow().points.len()
+    }
+
+    fn num_faces(&self) -> usize {
+        self.data.borrow().face_halfedge.len()
+    }
+
+    fn new_edge(self : &Rc<Self>, v1 : &Vertex, v2 : &Vertex) -> Halfedge {
+        let mut data = self.data.borrow_mut();
+        let n = data.next_halfedge.len();
+
+        let he = Halfedge { id : n + 0, mesh : self.clone() };
+        let op = Halfedge { id : n + 1, mesh : self.clone() };
+
+        data.halfedge_face.extend([std::u32::MAX; 2]);
+        data.halfedge_vertex.extend([v1.id as u32, v2.id as u32]);
+        data.next_halfedge.extend([std::u32::MAX; 2]);
+        data.prev_halfedge.extend([std::u32::MAX; 2]);
+
+        he
+    }
+
+    fn new_face(self : &Rc<Self>) -> Face {
+        let mut data = self.data.borrow_mut();
+        let id = data.face_halfedge.len();
+        data.face_halfedge.push(std::u32::MAX);
+        Face{ id, mesh : self.clone() }
+    }
+
+    fn set_vertex_halfedge(self : &Rc<Self>, v : &Vertex, he : &Halfedge) {
+        self.data.borrow_mut().vertex_halfedge[v.id] = he.id as u32;
+    }
+
+    fn set_next_and_prev(self : &Rc<Self>, he_prev : &Halfedge, he_next : &Halfedge) {
+        let mut data = self.data.borrow_mut();
+        data.next_halfedge[he_prev.id] = he_next.id as u32;
+        data.prev_halfedge[he_next.id] = he_prev.id as u32;
+    }
+
+    fn set_halfedge_face(self : &Rc<Self>, he : &Halfedge, f : &Face) {
+        self.data.borrow_mut().halfedge_face[he.id] = f.id as u32;
+    }
+
+    fn set_halfedge_vertex(self : &Rc<Self>, he : &Halfedge, v : &Vertex) {
+        self.data.borrow_mut().halfedge_vertex[he.id] = v.id as u32;
+    }
+
+    fn adjust_outgoing_halfedge(self : &Rc<Self>, v : &Vertex) {
+        for he in v.outgoing_halfedges() {
+            if he.is_boundary() {
+                self.set_vertex_halfedge(v, &he);
+            }
+        }
+    }
+}
